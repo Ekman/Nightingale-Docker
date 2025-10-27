98bfe43996460acc4430616c0168dfcf84b1c34f
Niklas Ekman <nikl.ekman@gmail.com>
2025-10-27 12:01:42 +0100

    Ensure correct permission on game dir

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,13 +2,13 @@
 
 set -e
 
+chown "$USER": -R "$GAME_DIR" "$CONFIG_DIR"
+
 gosu "$USER" "$STEAMCMDDIR/steamcmd.sh" \
     +force_install_dir "$GAME_DIR" \
     +login anonymous \
     +app_update "$GAME_ID" \
     validate \
     +quit
-
-chown "$USER": -R "$CONFIG_DIR"
 
 gosu "$USER" bash "$GAME_DIR/NWXServer.sh" $@
